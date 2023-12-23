8ac7cf1498d6981cd0c3d952d449f0051820a45f
arinorina <90761720+arinorina@users.noreply.github.com>
2023-12-23 11:06:31 +0300

    starting

---
+++ src/styles/theme.ts
@@ -0,0 +1,80 @@
+import { createGlobalStyle } from "styled-components";
+
+export const darkTheme = {
+  backgroundColor: "#3B2B4E",
+  textColor: "#FEBD64",
+  infoIconColor: "#FEBD64",
+  iconColor: "#76465F",
+  accentColor1: "#C0726D",
+  accentColor2: "#FEBD64",
+  shadowColor: "rgba(0,0,0,0.2)",
+
+  textSizeTitle: "25px",
+  textSizeTitle900: "22px",
+  textSizeTitle700: "22px",
+  textSizeTitle500: "22px",
+
+  textSizeTextL: "22px",
+  textSizeTextL900: "20px",
+  textSizeTextL700: "20px",
+  textSizeTextL500: "20px",
+
+  textSizeTextM: "16px",
+  textSizeTextM900: "14px",
+  textSizeTextM700: "14px",
+  textSizeTextM500: "14px",
+
+  textSizeTextS: "12px",
+  textSizeTextS900: "10px",
+  textSizeTextS700: "10px",
+  textSizeTextS500: "10px",
+
+  borderRadius: "15px",
+};
+
+export const lightTheme = {
+  backgroundColor: "#FFB381",
+  textColor: "#291F35",
+  infoIconColor: "#76465F",
+  iconColor: "#C0726D",
+  accentColor1: "#291F35",
+  accentColor2: "#76465F",
+  shadowColor: "rgba(0,0,0,0.2)",
+
+  textSizeTitle: "25px",
+  textSizeTitle900: "22px",
+  textSizeTitle700: "22px",
+  textSizeTitle500: "22px",
+
+  textSizeTextL: "22px",
+  textSizeTextL900: "20px",
+  textSizeTextL700: "20px",
+  textSizeTextL500: "20px",
+
+  textSizeTextM: "16px",
+  textSizeTextM900: "14px",
+  textSizeTextM700: "14px",
+  textSizeTextM500: "14px",
+
+  textSizeTextS: "12px",
+  textSizeTextS900: "10px",
+  textSizeTextS700: "10px",
+  textSizeTextS500: "10px",
+
+  borderRadius: "15px",
+};
+
+export const GlobalStyle = createGlobalStyle`
+  a{
+    font-family: 'Roboto';
+    text-decoration: none;
+    color: ${(props) => props.theme.textColor};
+  }
+  body{
+    margin: 0;
+    padding: 0;
+    background-color: ${(props) => props.theme.backgroundColor};
+    transition: 0.1s;
+    color: ${(props) => props.theme.textColor};
+  }
+`;
